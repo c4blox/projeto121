27262f4824d310523e0e31daa6ebe340a569dae4
c4blox <125309368+c4blox@users.noreply.github.com>
2024-03-28 15:51:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function takeSnapshot() {
     Webcam.snap(function (data_uri) { document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>'; })
 }
 console.log("ml5 version", ml5.version)
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7pDY4RwiZ/model.json', modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qQJjUmxL0/model.json', modelLoaded)
 function modelLoaded() {
     console.log("model loaded")
 }
